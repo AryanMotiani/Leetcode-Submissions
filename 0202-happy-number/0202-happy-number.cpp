c0f78f8cbabacc9727cadafe233b66dcadbccd7a
Aryan Motiani <aryanmotiani2006@gmail.com>
2025-07-31 09:22:31 +0530

    Time: 1 ms (33.89%), Space: 8.2 MB (63.96%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        long long a=n,b=0;
+        set<int> st;
+        while(a!=1)
+        {   
+            long long tmp=a;
+            while(tmp>0){
+                b+=pow(tmp%10,2);
+                tmp/=10;
+                if(b>LLONG_MAX/10) return false;
+            }
+            if(st.find(b)!=st.end()) return false;
+            st.insert(b);
+            a=b;
+            b=0;
+        }
+        return true;
+    }
+};
