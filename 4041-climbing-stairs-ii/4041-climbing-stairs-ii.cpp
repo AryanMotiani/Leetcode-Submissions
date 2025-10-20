e5e87adf80281353b589e05a194b0dd4f0a9809e
Aryan Motiani <aryanmotiani2006@gmail.com>
2025-10-21 00:44:23 +0530

    Time: 31 ms (37.67%), Space: 187.5 MB (22.18%) - LeetHub

--- 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    vector<long long> dp;
     int climbStairs(int n, vector<int>& costs) {
-       vector<int> dp(n+1);
-       dp[0]=0;
-       dp[1]=1+costs[0];
-       if(n>1) dp[2]=min(4+dp[0],1+dp[1])+costs[1]; // costs has 0 based indexing unlike said in the Q
-       for(int i=3;i<=n;i++){
-            dp[i]=min(9+dp[i-3],min(4+dp[i-2],1+dp[i-1]))+costs[i-1];
-       }
-       return dp[n];
+        if(dp.empty()) dp.assign(n + 1, -1); // cannot declare globally coz need to keep all values -1 but in leetcode it does not reset
+    // main bug during coding: costs is 0 based indexed
+        if(dp[n]!=-1) return dp[n];
+        if(n==0) return dp[0]=0;
+        if(n==1) return dp[1]=1+costs[0];
+        if(n==2) return dp[2]=min(4+climbStairs(n-2,costs)+costs[n-1],1+climbStairs(n-1,costs)+costs[n-1]);
+       
+        long long c1=9+climbStairs(n-3,costs)+costs[n-1];
+        long long c2=4+climbStairs(n-2,costs)+costs[n-1];
+        long long c3=1+climbStairs(n-1,costs)+costs[n-1];
+        return dp[n]=min(c1,min(c2,c3));
     }
 };
