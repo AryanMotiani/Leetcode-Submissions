efec0b4bcab89a8ab4980bec1426926b4b54717c
Aryan Motiani <aryanmotiani2006@gmail.com>
2025-10-20 13:50:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,13 @@
+int N=46;
+vector<int> dp(N,-1);
+
 class Solution {
 public:
+
     int climbStairs(int n) {
-        int twos=n/2,ans=0;
-        while(twos>=0){
-            int ones=n-(2*twos);
-            int mn=min(ones,twos);
-            long long tmp=1;
-            int total=ones+twos;
-            for(int i=1;i<=mn;i++){
-                tmp*=(total-i+1);
-                tmp/=i;
-            }
-            ans+=tmp;
-            twos--;
-        }
-        return ans;
+        if(dp[n]!=-1) return dp[n];
+        if(n==0) return 1;
+        if(n==1) return 1;
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
 };
